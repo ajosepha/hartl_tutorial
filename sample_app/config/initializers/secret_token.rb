a913db465f883ce4ed12450400539140096ca163
ajosepha <ariel.isaacs@gmail.com>
2014-01-10 11:09:15 -0500

    add sample_app to practice static pages

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '95f942c5162e2ebec748b3451d131cd00b12439d758cb944fe7cce9e7b90be1e0a6641c94a2b4e4e6907c4301ece9b741a9641c58b3ff29e49554bdf8a254ff4'
